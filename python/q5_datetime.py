eab0f53eae8735449176acb9025a5ecb3aa8d86c
Andy <andysevas@users.noreply.github.com>
2016-03-29 10:58:12 -0700

    Finished datetime

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -4,20 +4,28 @@
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
 
+import datetime
 a = '01-02-2013'
 b = '07-28-2015'
 c=datetime.datetime.strptime(a, "%m-%d-%Y").date()
 d=datetime.datetime.strptime(b, "%m-%d-%Y").date()
-(d-c).days
+(d-c).days  
+938 is the answer.
 
 
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
-
-
+e=datetime.datetime.strptime(date_start, "%m%d%Y").date()
+f=datetime.datetime.strptime(date_stop, "%m%d%Y").date()
+(f-e).days
+513 is the answer.
 
 
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+g=datetime.datetime.strptime(date_start, "%d-%b-%Y").date()
+h=datetime.datetime.strptime(date_stop, "%d-%b-%Y").date()
+(h-g).days
+7850 is the answer.
