9c398463e9d790f04994650e7137c4fde4894d75
Andy <andysevas@users.noreply.github.com>
2016-03-29 10:53:13 -0700

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -4,9 +4,19 @@
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
 
+a = '01-02-2013'
+b = '07-28-2015'
+c=datetime.datetime.strptime(a, "%m-%d-%Y").date()
+d=datetime.datetime.strptime(b, "%m-%d-%Y").date()
+(d-c).days
+
+
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
+
+
+
 
 ####c)  
 date_start = '15-Jan-1994'  
