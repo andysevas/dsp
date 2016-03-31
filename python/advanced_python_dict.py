2c0086502b026213fe63e8e4551f8815eee5ea54
Andy <andysevas@users.noreply.github.com>
2016-03-31 09:37:40 -0700

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,11 @@
-PLACE YOUR CODE HERE
+
+#The value 'data' is from the last problem.  It's the faculty.csv data.
+faculty_dict = {}
+for i in data:
+        fullname = i[0].split()
+        lastname = fullname[-1]
+        faculty_dict[lastname] = row[1:]
+
+#Question 6
+first3pairs = {k: faculty_dict[k] for k in faculty_dict.keys()[:3]}
+
