83130569395dbe3bd7facb00f0d0ae019da9e8d2
Andy <andysevas@users.noreply.github.com>
2016-03-31 09:15:11 -0700

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,16 @@
-PLACE YOUR CODE HERE
+
+import csv
+import collections
+
+with open('faculty.csv') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+    
+
+emails  = [i[3] for i in data if i[3] != data[0][3]]
+
+emailfile = open('emails.csv', 'wb')
+writer = csv.writer(emailfile)
+for i in emails:
+    writer.writerow([i])
+emailfile.close()
